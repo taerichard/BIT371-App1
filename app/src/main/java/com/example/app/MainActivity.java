f8f43ae6a901e604dfb5347beb7d2c9695e607fb
Richard <taerichard@gmail.com>
2020-10-11 00:26:31 -0700

    created userConfirmationActivity to check username/pw

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.enter_button);
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView pw = (TextView) findViewById(R.id.password);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
+                String userName = username.getText().toString();
+                String password = pw.getText().toString();
+                intent.putExtra("username", userName);
+                intent.putExtra("password", password);
+                startActivity((intent));
+
+            }
+        });
+
     }
 }
+
+
