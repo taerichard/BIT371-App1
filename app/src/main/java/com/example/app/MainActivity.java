66894ac903f658156a08e815e3ab4e3b7b7aa438
Richard <taerichard@gmail.com>
2020-10-18 17:55:27 -0700

    created arrayAdapter for city list and connected to listView

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,37 +1,25 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood", "Renton",
+            "Redmond", "Spokane", "Vancouver", "Tacoma", "Olympia", "Bellingham", "Arlington",
+            "Everett", "Woodinville", "Monroe", "New Castle", "Ballard", "Lacey"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.enter_button);
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView pw = (TextView) findViewById(R.id.password);
-
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
-                String userName = username.getText().toString();
-                String password = pw.getText().toString();
-                intent.putExtra("username", userName);
-                intent.putExtra("password", password);
-                startActivity((intent));
-
-            }
-        });
-
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.city, cities);
+        ListView listView = findViewById(R.id.city_list);
+        listView.setAdapter(arrayAdapter);
     }
 }
 
